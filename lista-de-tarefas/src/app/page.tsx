db967819e17004539c3e24cdaf9d07d0bc59eb69
Essential <Essential@MacBook-Pro-de-essentia-2.local>
2024-05-20 17:06:00 -0300

    Adicionada função de dar check nas tarefas

--- lista-de-tarefas/src/app/page.tsx
+++ lista-de-tarefas/src/app/page.tsx
@@ -8,7 +8,7 @@ const Page = () => {
   const [itemInput, setItemInput] = useState('')
 
   const [list, setList] = useState<TodoItem[]>([
-    { label: 'Fazer dever de casa', checked: false },
+    { label: 'Fazer dever de casa', checked: true },
     { label: 'Comprar bolo', checked: false },
   ])
 
@@ -24,6 +24,12 @@ const Page = () => {
     )
   }
 
+  const toggleItem = (index: number) => {
+    let newList = [...list]
+    newList[index].checked = !newList[index].checked
+    setList(newList)
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col items-center text-2xl">
       <h1 className="text-4xl mt-5">Lista de tarefas</h1>
@@ -35,12 +41,15 @@ const Page = () => {
           value={itemInput}
           onChange={e => setItemInput(e.target.value)}
         />
-        <button onClick={handleAddButton}>Adicionar</button>
+        <button className="bg-green-700 p-3 rounded-md" onClick={handleAddButton}>Adicionar</button>
       </div>
       <p className="my-4">{list.length} itens na lista</p>
-      <ul className="w-full max-w-lg list-disc pl-5">
+      <ul className="w-full max-w-lg pl-5">
         {list.map((item, index) =>
-          <li>{item.label} - <button onClick={() => deleteItem(index)} className="hover:underline">[ deletar ]</button></li>
+          <li>
+            <input onClick={() => toggleItem(index)} type="checkbox" checked={item.checked} className="w-5 h-5 mr-3" />
+            {item.label} - <button onClick={() => deleteItem(index)} className="bg-red-700 p-1 rounded-md text-sm hover:underline">Deletar</button>
+          </li>
         )}
       </ul>
     </div>
